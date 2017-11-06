d9cf65fc8540bd3b9a7e6cf1b04091d2179e722e
Kevin Pak <kedpak@yahoo.com>
2017-11-05 17:51:58 -0800

    add google map api to components

--- react/src/resultBox.js
+++ react/src/resultBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import Result from './result';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class ResultBox extends Component {
   constructor(props){
@@ -11,7 +12,9 @@ class ResultBox extends Component {
       posts: [],
       eventId: "default",
       eventDes: "default",
-      eventTime: "default"
+      eventTime: "default",
+      latitude: "default",
+      longitude: "default"
     };
     this.click = this.click.bind(this);
     this.togglePopup = this.togglePopup.bind(this);
@@ -28,25 +31,47 @@ class ResultBox extends Component {
         this.setState({posts});
       });
     }
-    click(id, des, time){
+    click(id, des, time, lat, long){
       this.setState({eventId: id,
                      eventDes: des,
-                     eventTime: time});
+                     eventTime: time,
+                     latitude: lat,
+                     longitude: long});
       this.togglePopup();
     }
-    buildRow() {
-      return (
-        this.state.posts.map(events =>
-          <div key={events.key} className='result_box'
-             onClick={ () => this.click(events.name, events.description, events.time)}
-             id={events._id}
-             >
-             <p>{events.name}</p>
-             {events._id}
-             {events.place && events.place.location && <p>{events.place.location.city}</p>}
-          </div>
-        )
-      )
+      buildRow() {
+        return this.state.posts
+        .filter(events =>
+            events.place
+            && events.place.location
+            && events.place.location.city === this.props.searchVal)
+        .map(events =>
+            <div key={events.key} className='result_box'
+                onClick={() => this.click(events.name, events.description,
+                events.time, events.place.location.latitude, events.place.location.longitude)}
+                id={events._id}
+
+                >
+                <p>{events.name}</p>
+                {events._id}
+                <p>{events.place.location.city}</p>
+                <div className="the_map" style={{overflow:'hidden'}}>
+                <Map google={this.props.google} zoom={14}
+                initialCenter={{
+                  lat: events.place.location.latitude,
+                  lng: events.place.location.longitude
+                }}
+                style={{width:'200px', height:'200px', position:'relative'}}>
+                  <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+                  <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+
+                    </div>
+                  </InfoWindow>
+                </Map>
+                </div>
+            </div>);
     }
     popUp() {
       return (
@@ -55,6 +80,21 @@ class ResultBox extends Component {
                   <p>{this.state.eventId}</p>
                   <p className="eventDesc">{this.state.eventDes}</p>
                   <button onClick={this.togglePopup.bind(this)}>close me</button>
+                  <Map google={this.props.google} zoom={14}
+                  initialCenter={{
+                    lat: this.state.latitude,
+                    lng: this.state.longitude
+                  }}
+                  style={{width:'600px', height:'600px', position:'relative'}}>
+                    <Marker onClick={this.onMarkerClick}
+                          name={'Current location'} />
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                      <div>
+
+                      </div>
+                    </InfoWindow>
+                  </Map>
+
                </div>
             </div>
       )
@@ -73,5 +113,6 @@ class ResultBox extends Component {
     );
   }
 }
-
-export default ResultBox;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDhc1bxQCcF6ecfAwC1Pj-Fv4OW9IZ3xbI'
+})(ResultBox)
