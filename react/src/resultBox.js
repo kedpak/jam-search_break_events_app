b400acb803c09db89feb9eb72545929ac8fb88d7
Kevin Pak <kedpak@yahoo.com>
2017-11-06 20:49:48 -0800

    change background color of inner popup and add date/street to description

--- react/src/resultBox.js
+++ react/src/resultBox.js
@@ -14,7 +14,8 @@ class ResultBox extends Component {
       eventDes: "default",
       eventTime: "default",
       latitude: "default",
-      longitude: "default"
+      longitude: "default",
+      eventPlace: "default"
     };
     this.click = this.click.bind(this);
     this.togglePopup = this.togglePopup.bind(this);
@@ -31,12 +32,13 @@ class ResultBox extends Component {
         this.setState({posts});
       });
     }
-    click(id, des, time, lat, long){
+    click(id, des, time, lat, long, place){
       this.setState({eventId: id,
                      eventDes: des,
                      eventTime: time,
                      latitude: lat,
-                     longitude: long});
+                     longitude: long,
+                     eventPlace: place});
       this.togglePopup();
     }
       buildRow() {
@@ -48,27 +50,25 @@ class ResultBox extends Component {
         .map(events =>
             <div key={events.key} className='result_box'
                 onClick={() => this.click(events.name, events.description,
-                events.time, events.place.location.latitude, events.place.location.longitude)}
+                events.start_time, events.place.location.latitude,
+                events.place.location.longitude, events.place.location.street)}
                 id={events._id}
-
                 >
                 <p>{events.name}</p>
-                {events._id}
+                {events.start_time.slice(0, 10)}
+
                 <p>{events.place.location.city}</p>
                 <div className="the_map" style={{overflow:'hidden'}}>
-                <Map google={this.props.google} zoom={14}
-                initialCenter={{
-                  lat: events.place.location.latitude,
-                  lng: events.place.location.longitude
-                }}
-                style={{width:'200px', height:'200px', position:'relative'}}>
-                  <Marker onClick={this.onMarkerClick}
+                     <Map google={this.props.google} zoom={14}
+                      initialCenter={{
+                      lat: events.place.location.latitude,
+                      lng: events.place.location.longitude
+                      }}
+                      style={{width:'200px', height:'200px', position:'relative'}}>
+                    <Marker onClick={this.onMarkerClick}
                         name={'Current location'} />
-                  <InfoWindow onClose={this.onInfoWindowClose}>
-                    <div>
-
-                    </div>
-                  </InfoWindow>
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                    </InfoWindow>
                 </Map>
                 </div>
             </div>);
@@ -77,9 +77,12 @@ class ResultBox extends Component {
       return (
             <div className='popup'>
                <div className='popup_inner'>
-                  <p>{this.state.eventId}</p>
+                  <p className="eventName">{this.state.eventId}</p>
+                  <p className="eventTime">{this.state.eventTime.slice(0, 10)}</p>
+                  <p className="eventPlace">{this.state.eventPlace}</p>
                   <p className="eventDesc">{this.state.eventDes}</p>
                   <button onClick={this.togglePopup.bind(this)}>close me</button>
+                  <div className="inner_map">
                   <Map google={this.props.google} zoom={14}
                   initialCenter={{
                     lat: this.state.latitude,
@@ -94,6 +97,7 @@ class ResultBox extends Component {
                       </div>
                     </InfoWindow>
                   </Map>
+                  </div>
 
                </div>
             </div>
