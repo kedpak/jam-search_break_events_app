f6baaee91319aeb03e38c731bb996acbe4dc9492
Kevin Pak <kedpak@yahoo.com>
2017-11-09 01:19:01 -0800

    add new search bar with auto discovery of location

--- react/src/App.js
+++ react/src/App.js
@@ -3,31 +3,49 @@ import './App.css';
 import Result from './result';
 import NavBar from './NavBar';
 import ResultBox from './resultBox';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       value: '',
-      status: false
+      status: false,
+      location: 'default',
+      lat: 'default',
+      lng: 'default'
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.click = this.click.bind(this);
-
-  }
+    this.onChange = (location) => this.setState({ location})
+    }
     handleChange(event) {
       this.setState({value: event.target.value});
     }
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
+    handleSubmit = (event) => {
+      event.preventDefault()
+      geocodeByAddress(this.state.location)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => this.setState({lat: latLng.lat, lng: latLng.lng}))
+      .catch(error => console.error('Error', error))
+
     }
     click() {
       this.setState({status: !this.state.status});
     }
   render() {
-    let resultbox = this.state.status ? <ResultBox searchVal={this.state.value} /> : null;
+    let resultbox = this.state.status ? <ResultBox searchVal={this.state.value}
+      searchLat={this.state.lat} searchLng={this.state.lng}/> : null;
+    const inputProps = {
+      value: this.state.location,
+      onChange: this.onChange,
+    }
+    const options = [
+        'one', 'two', 'three'
+      ]
+      const defaultOption = options[0]
     return (
       <div className="App">
       <form className="form-body" onSubmit={this.handleSubmit}>
@@ -35,18 +53,16 @@ class App extends Component {
            Jam Search
          </h2>
         <label className="nav_lab">
-           search location:
-          <input type="text" value={this.state.value} onChange={this.handleChange} className="searchBox"/>
-          <input type="submit" value="Submit" onClick={this.click} className="sub_button"/>
+            search location:
+          <PlacesAutocomplete className="auto" inputProps={inputProps} />
         </label>
-
+        <input type="submit" value="Submit" onClick={this.click} className="sub_button"/>
+        <button className="postEvent">Post Event!</button>
       </form>
           <div className="spacer"></div>
-          <p className="App-intro">
-            Find a jam
-          </p>
+
           {resultbox}
-          </div>
+    </div>
     );
   }
 }
